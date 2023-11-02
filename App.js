9fad5d86fb7d2c2cc4f0f683767eef093b9cc4bd
DESKTOP-DU2RDFE\Admin <luudat214@gmail.com>
2023-11-02 21:52:54 +0700

    login

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLogin = () => {
+    setShowLogin(true);
+
+    if (username === 'admin' && password === '123') {
+      setShowForm(true);
+      setShowAlert(false);
+    } else {
+      setShowForm(false);
+      setShowAlert(true);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!showLogin && (
+        <View>
+          <Button title="Login" onPress={() => setShowLogin(true)} />
+        </View>
+      )}
+      {showLogin && !showForm && (
+        <View style={styles.alertContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Username"
+            onChangeText={(text) => setUsername(text)}
+            value={username}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+          {showAlert && (
+            <Text style={styles.alertText}>Thông tin tài khoản không chính xác</Text>
+          )}
+          <Button title="Đăng nhập" onPress={handleLogin} />
+        </View>
+      )}
+      {showForm && (
+        <View>
+          <Text style={styles.alertText}>Đăng nhập thành công!</Text>
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +66,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  alertContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  alertText: {
+    color: 'red',
+    marginBottom: 10,
+  }
 });
