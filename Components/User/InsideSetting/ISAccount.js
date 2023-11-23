3557c0277d308a080c2ae299f465ffb0619161df
MGCThero <luudat214@gmail.com>
2023-11-23 16:33:04 +0700

    update ISAccount

--- Components/User/InsideSetting/ISAccount.js
+++ Components/User/InsideSetting/ISAccount.js
@@ -21,7 +21,7 @@ const ISAccount = () => {
                     <Text style={styles.text}>Tài khoản</Text>
                 </View>
 
-                <TouchableOpacity onPress={handleNothing}>
+                <TouchableOpacity onPress={() => navigation.navigate('Mychannel')}>
                     <View style={styles.viewfix}>
                         <View style={styles.viewtext1}>
                             <Text style={styles.textup}>
@@ -32,10 +32,37 @@ const ISAccount = () => {
                             </Text>
                         </View>
                         <View style={styles.icon}>
-                            <AntDesign name="right" size={20} color="#EAEAEC" />
+                            <AntDesign name="right" size={24} color="#57575C" />
                         </View>
                     </View>
                 </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleNothing}>
+                    <View style={styles.viewlogout}>
+                        <Text style={styles.textlogout}>
+                            Đăng xuất khỏi Amazon Prime
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+
+                <View style={styles.viewdltAP}>
+                    <Text style={styles.textdltAP}>
+                        Xoá mọi thông tin đăng nhập Amazon Prime đã lưu.
+                    </Text>
+                </View>
+                <View style={styles.viewdanger}>
+                    <View style={[styles.viewDnD, styles.viewDnDT]}>
+                        <Text style={styles.textDnD}>
+                            Vô hiệu hoá tài khoản
+                        </Text>
+                    </View>
+
+                    <View style={[styles.viewDnD, styles.viewDnDB]}>
+                        <Text style={styles.textDnD}>
+                            Xoá tài khoản vĩnh viễn
+                        </Text>
+                    </View>
+                </View>
             </View>
 
 
@@ -70,15 +97,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         marginRight: 30,
     },
-
-
-
-
-
     viewtext1: {
         flex: 1,
     },
-
     textup: {
         marginTop: 13,
         flex: 1,
@@ -89,11 +110,8 @@ const styles = StyleSheet.create({
     },
     textdown: {
         color: '#A6A5AA',
-        // fontSize: 20,
-
         marginLeft: 15,
     },
-
     viewfix: {
         backgroundColor: '#18171C',
         height: 70,
@@ -102,16 +120,58 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderRadius: 10
     },
-
     icon: {
         justifyContent: 'flex-end',
         marginRight: 20,
+        marginTop: 10,
     },
+    viewlogout: {
+        backgroundColor: '#18181B',
+        justifyContent: "center",
+        height: 50,
+        marginTop: 30,
+        borderRadius: 10,
+    },
+    textlogout: {
+        color: '#7F65A8',
+        marginLeft: 15,
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    viewdltAP: {
+        marginTop: 10,
+        marginBottom: 25,
+        paddingLeft: 30,
+        paddingRight: 30
+    },
+    textdltAP: {
+        color: '#929299'
+    },
+    viewdanger: {
+        backgroundColor: '#18181B',
+        height: 100,
+        borderRadius: 10,
 
-
-
-
-
+    },
+    viewDnD: {
+        backgroundColor: '#18181B',
+        height: 50,
+        justifyContent: "center",
+        marginLeft: 15,
+    },
+    viewDnDT: {
+        borderBottomColor: '#3B3B3D',
+        borderBottomWidth: 1,
+        borderTopRightRadius: 10,
+    },
+    viewDnDB: {
+        borderBottomRightRadius: 10,
+    },
+    textDnD: {
+        color: '#C11917',
+        fontSize: 16,
+        fontWeight: "bold",
+    },
 
 });
 
